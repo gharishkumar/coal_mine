aeb017993d2fef9a5ecd826eb309e022e2c5d4e5
Harish Kumar <g.harishkumar1998@gmail.com>
2021-03-19 07:21:43 +0530

    blynk 🤕 ☑️

--- Reciver/Reciver.ino
+++ Reciver/Reciver.ino
@@ -1,66 +1,69 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
-#include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
-LiquidCrystal_I2C lcd(0x27,16,2); 
+LiquidCrystal_I2C lcd(0x27,16,2);
 SoftwareSerial bluetooth(14,12);
+BlynkTimer timer;
 
 const int buzzer = 17;
+float humidity, temperature;
+int rainSensor, gasSensor;
 bool state = true;
+String sensorValues;
 
-char auth[] = "YourAuthToken";
+char auth[] = "yourBlynkToken";
 
-char ssid[] = "YourNetworkName";
-char pass[] = "YourPassword";
+char ssid[] = "yourSSID";
+char pass[] = "yourPassword";
 
-void setup()
+String getValue(String data, char separator, int index)
 {
-  lcd.init();
-  lcd.backlight();
-  lcd.clear();
-  pinMode(buzzer, OUTPUT);
-  Serial.begin(9600);
-  bluetooth.begin(9600);
-  Blynk.begin(auth, ssid, pass);
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
-
-void loop()
-{
-  Blynk.run();
-  StaticJsonBuffer<1000> jsonBuffer;
-  JsonObject& json = jsonBuffer.parseObject(bluetooth);
-  //json.prettyPrintTo(Serial);
-  if (json == JsonObject::invalid())
-  return;
-  Serial.println("JSON received and parsed");
+void sensorDataRead(){
+  if (bluetooth.available() > 0) {
+    sensorValues = bluetooth.readString();
+  }
+  else
+  {
+    return;
+  }
+  humidity=getValue(sensorValues, ':', 0).toFloat();
+  temperature=getValue(sensorValues, ':', 1).toFloat();
+  rainSensor=getValue(sensorValues, ':', 2).toInt();
+  gasSensor=getValue(sensorValues, ':', 3).toInt();
+  Serial.println("sensorValues received and parsed");
   Serial.print("\n");
   Serial.print("Humidity ");
-  float humidity=json["humidity"];
   Serial.print(humidity);
   Serial.println("%");
   Serial.print("Temperature ");
-  float temperature=json["temperature"];
   Serial.print(temperature);
   Serial.println("*C");
   Serial.print("RainSensorValue ");
-  int rainSensor=map(json["rainSensorValue"], 0, 1024, 0, 100);
   Serial.print(rainSensor);
   Serial.println("%");
   Serial.print("GasSensorValue ");
-  int gasSensor=map(json["gasSensorValue"], 0, 1024, 0, 100);
   Serial.print(gasSensor);
   Serial.println("%");
   Serial.println("---------------------xxxxx--------------------");
-  Blynk.virtualWrite(V1, humidity);
-  Blynk.virtualWrite(V2, temperature);
-  Blynk.virtualWrite(V3, rainSensor);
-  Blynk.virtualWrite(V4, gasSensor);
   lcd.setCursor(0, 0);
   lcd.print("T:");
-  lcd.print(temperature);
+  lcd.print(temperature,1);
   lcd.write(223);
   lcd.print("C ");
   lcd.print("GAS:");
@@ -76,9 +79,10 @@ void loop()
   {
     lcd.print("NO WATER PERSENT");
   }
-  if ((temperature > 40.0) | (gasSensor > 70) | (rainSensor > 70))
+  if ((temperature > 40.0) || (gasSensor > 70) || (rainSensor > 70))
   {
     digitalWrite(buzzer,HIGH);
+    Serial.println("---------------------Warning--------------------");
     if (state)
     {
       Blynk.notify("Warning limits exceded safety range");
@@ -90,4 +94,33 @@ void loop()
     digitalWrite(buzzer,LOW);
     state = true;
   }
+  Blynk.virtualWrite(V1, (int)humidity);
+  Blynk.virtualWrite(V2, (int)temperature);
+  Blynk.virtualWrite(V3, rainSensor);
+  Blynk.virtualWrite(V4, gasSensor);
+}
+void setup()
+{
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  pinMode(buzzer, OUTPUT);
+  Serial.begin(9600);
+  bluetooth.begin(38400);
+  lcd.setCursor(0, 0);
+  lcd.print("Connecting to   ");
+  lcd.setCursor(0, 1);
+  lcd.print("blynk server    ");
+  Blynk.begin(auth, ssid, pass);
+  timer.setInterval(1000L, sensorDataRead);
+  lcd.setCursor(0, 0);
+  lcd.print("Connected to    ");
+  lcd.setCursor(0, 1);
+  lcd.print("blynk server    ");
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
 }
